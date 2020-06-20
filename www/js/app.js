107b38cf79615d1f8bc961fa8efd092515c9ff9c
sidab <39728359+sidab@users.noreply.github.com>
2020-06-20 11:31:12 +0300

    fix

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@ var app = new Framework7({
     root: '#app',
     name: 'Delivery',
     theme: 'ios',
-    version: 2.4,
+    version: 2.5,
     routes: routes,
     init: false,
     user: localStorage.user ? localStorage.user : false,
