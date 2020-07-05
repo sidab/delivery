944e42fae08161d45b609048941b20fd9ed292bd
sidab <39728359+sidab@users.noreply.github.com>
2020-07-05 10:13:55 +0300

    fix

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@ var app = new Framework7({
     root: '#app',
     name: 'Zaytoon',
     theme: 'ios',
-    version: 3.2,
+    version: 3.3,
     routes: routes,
     init: false,
     user: localStorage.user ? localStorage.user : false,
